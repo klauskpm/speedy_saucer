3dd711347c90e84c79cf177c9b406bd203d1542a
Klaus Kazlauskas <klausmachado@gmail.com>
2024-06-02 17:05:40 -0400

    first commit

---
+++ player.gd
@@ -0,0 +1,15 @@
+extends RigidBody2D
+
+@export
+var  FORCE = 1000
+
+func _physics_process(delta):
+	
+	if (Input.is_action_pressed("move_right")):
+		apply_force(Vector2(FORCE, 0))
+	elif (Input.is_action_pressed("move_left")):
+		apply_force(Vector2(-FORCE, 0))
+	elif (Input.is_action_pressed("move_up")):
+		apply_force(Vector2(0, -FORCE))
+	elif (Input.is_action_pressed("move_down")):
+		apply_force(Vector2(0, FORCE))
