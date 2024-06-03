812d01ff6f0771e8e8f180c217a2f4d81af940c4
Klaus Kazlauskas <klausmachado@gmail.com>
2024-06-02 20:10:00 -0400

    add dynamic tutorial text with translations

--- level.gd
+++ level.gd
@@ -3,10 +3,50 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	TranslationServer.set_locale("en")
+	var actions = get_actions_as_tutorial_string()
+	var label = $MovementTutorialLabel
+	label.text = label.text.format({ 'to_replace': actions})
+	
+	
 
 func _on_maze_body_exited(_body):
-	call_deferred("resetLevel");
+	call_deferred("reset_level");
 
-func resetLevel():
+func reset_level():
 	get_tree().reload_current_scene()
+	
+func get_actions_as_tutorial_string():
+	var list = []
+	var actions_dict = get_custom_actions_dict()
+	for action in actions_dict.keys():
+		
+		var events = ' or '.join(actions_dict.get(action))
+		print(action)
+		list.append(tr(action) + ': ' + events)
+		
+	return '\n'.join(list)
+
+func get_custom_actions_list():
+	var actions = []
+	for action in InputMap.get_actions():
+		if action.contains('custom__'):
+			actions.append(action)
+			
+	return actions
+
+func get_action_events(action: String):
+	var events = []
+	var action_events = InputMap.action_get_events(action)
+	for ev in action_events:
+		events.append(tr("event_" + ev.as_text()))
+		
+	return events
+	
+func get_custom_actions_dict():
+	var list = get_custom_actions_list()
+	var actions_dict = {}
+	for item in list:
+		actions_dict[item] = get_action_events(item)
+		
+	return actions_dict
--- player.gd
+++ player.gd
@@ -4,11 +4,11 @@ extends RigidBody2D
 
 func _physics_process(_delta):
 	
-	if (Input.is_action_pressed("move_right")):
+	if (Input.is_action_pressed("custom__move_right")):
 		apply_force(Vector2(FORCE, 0))
-	elif (Input.is_action_pressed("move_left")):
+	elif (Input.is_action_pressed("custom__move_left")):
 		apply_force(Vector2(-FORCE, 0))
-	elif (Input.is_action_pressed("move_up")):
+	elif (Input.is_action_pressed("custom__move_up")):
 		apply_force(Vector2(0, -FORCE))
-	elif (Input.is_action_pressed("move_down")):
+	elif (Input.is_action_pressed("custom__move_down")):
 		apply_force(Vector2(0, FORCE))
